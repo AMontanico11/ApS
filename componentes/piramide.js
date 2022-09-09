71362276d5c1eb68f20e93439cb8e9cb6f6a8439
Gerardo <ogjordonez@scl.edu.gt>
2022-09-09 08:33:48 -0600

    Se agregan los archivos js

---
+++ componentes/piramide.js
@@ -0,0 +1,10 @@
+export let piramide1 = `<div class="Integrantes">
+<div>Integrantes <br> Biologia: <br> Karen Amanda Calderas de León <br> Jaydi Dayana Orrego Perez <br> Angel Gabriel Castro</div>
+<div>Integrantes <br> Diseño Gráfico: <br> Detlef Ivan Iquique Galvez <br> Hannah Maribel Mijangos</div>
+<div>Integrantes <br> Computación: <br> Oscar Gerardo de Jesus Ordoñez Alvarez <br> Alexis Fabian Pirir Montanico</div>
+
+<div>Empresa: <br> Healthy Point</div>
+<div>Paginas de referencia: <br> Nutrium <br> Pagina de inicio con Genshin Impact</div>
+</div>
+
+`;
