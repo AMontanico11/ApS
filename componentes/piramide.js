a963f304a85ccbbf3a655e5093406fd38b848f32
AMontanico11 <80012647+AMontanico11@users.noreply.github.com>
2022-09-21 16:53:19 -1200

    Update piramide.js

--- componentes/piramide.js
+++ componentes/piramide.js
@@ -6,17 +6,89 @@ export function ProteinaDesgloce(){
             '<img class="banner" src="./img/nutria.2 1.png" alt="">'+
             '<a class="primer" href="">Inicio</a>' +
             '<a class="segundo" href="">Calcular</a>' +
-            '</header>',
+            '</header>'+
+
+            '<img class="menu" src="https://github.com/Gerardo072/Img-Aps-/blob/main/Frutas.png?raw=true" alt="">'+
+            '<h1 class="tittle2">¿Qué son las proteínas?</h1>'+
+    
 
 
+
+            '<div class="Sector">'+
+            '<p>'+
+            'Las proteínas están formadas por muchos bloques de construcción, '+
+            'conocidos como <br> aminoácidos para suministrar aminoácidos que el crecimiento '+
+            'y mantenimiento de <br> nuestras células y tejidos.  Las proteínas de origen vegetal '+
+            'y animal varían en su calidad <br> y digestibilidad, están formadas por muchos '+
+            'aminoácidos diferentes unidos entre sí. Hay <br> veinte bloques de construcción de '+
+            'aminoácidos diferentes que se encuentran <br> comúnmente en plantas y animales. '+
+            'Una proteína típica está compuesta de 300 o más <br> aminoácidos y la secuencia '+
+            'y el número específicos de aminoácidos son únicos para cada <br> proteína.'+
+            '</p>'+
+            '</div>'+
+
+
+
+
+            '<img class="fon-inferior" src="https://github.com/AMontanico11/IMGs-ApS/blob/main/image%2020.png?raw=true" alt="">'+
+    
+            '<section class="background"></section>'+
+    
+            '<div class="Tabla-1">'+
+        
+            '<table> '+
+            '<tr><td class="Sect1">Alimentos</td> <td class="Sect2">Proteínas</td> <td class="Sect3">Vitaminas</td> <td class="Sect4">Lípidos</td> <td class="Sect5">Carbohidratos</td> <td class="Sect6">Prueba de barra1</td> <td>Prueba de barra1</td> <td>Prueba de barra1</td> <td>Prueba de barra1</td> <td>Prueba de barra1</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '<tr><td class="Sect1">1</td><td class="Sect2">2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>'+
+            '</table>'+
+            '</div>'+
+            '<h1 class="tittle">Frutas</h1>'+
+
+
+            '<div class="Dieta">'+
+            '<div class="Dieta-text">'+
+            '<h1>Dieta Recomendada</h1>'+
+            
+            '</div>'+
+
+            '</div>'+
+
+
+
+            '<div class="Integrantes">'+
+            '<div>Integrantes <br> Biologia: <br> Karen Amanda Calderas de León <br> Jaydi Dayana Orrego Perez <br> Angel Gabriel Castro</div>'+
+            '<div>Integrantes <br> Diseño Gráfico: <br> Detlef Ivan Iquique Galvez <br> Hannah Maribel Mijangos</div>'+
+            '<div>Integrantes <br> Computación: <br> Oscar Gerardo de Jesus Ordoñez Alvarez <br> Alexis Fabian Pirir Montanico</div>'+
+    
+            '<div>Empresa: <br> Healthy Point</div>'+
+            '<div>Paginas de referencia: <br> Nutrium <br> Pagina de inicio con Genshin Impact</div>'+
+            '</div>',
+
+            
+        
     }
     ]
-
+    
     let Proteina = document.querySelector("#Proteina")
-
+     
      function Desglosar(){
         root.innerHTML = Despliegue[0].Piramide;
-
+        
      }
 
      Proteina.addEventListener("click", Desglosar)
