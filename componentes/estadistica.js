347685c35d2ada1ea007cdc4c18747818de607fc
Montanico <afpirir@scl.edu.gt>
2022-09-21 20:47:38 -0600

    Se actualizan las partes de menu

--- componentes/estadistica.js
+++
@@ -1,6 +0,0 @@
-export  let header1 = 
-    `<header>
-    <img class="banner" src="./img/nutria.2 1.png" alt="">
-    <a class="primer" href="">Inicio</a>
-    <a class="segundo" href="">Calcular</a>
-</header>`;
